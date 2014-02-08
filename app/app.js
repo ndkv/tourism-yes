08ab7a1c3024e6382d19bf6bcd69f743d09e5757
Simeon Nedkov <simeon@ndkv.nl>
2014-02-08 12:11:03 +0100

    add fullscreen control

--- app/app.js
+++
@@ -1,5 +0,0 @@
-window.onload = run;
-
-function run() {
-    var vis = cartodb.createVis('map', 'http://simeon.cartodb.com/api/v2/viz/46345544-6d62-11e3-99da-5404a6a69006/viz.json');
-    }
---
+++ app/src/app.js
@@ -0,0 +1,42 @@
+window.onload = run;
+
+function run() {
+    var map = new L.map('map', {zoomControl: false});
+	map.setView([42.5657, 25.5123], 7);
+	
+	L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+          attribution: 'Stamen'
+    }).addTo(map);
+	
+	cartodb.createLayer(map, 'http://simeon.cartodb.com/api/v2/viz/46345544-6d62-11e3-99da-5404a6a69006/viz.json')
+	.addTo(map)
+	.on('done', function (layer) {
+	    layer.setInteraction(true);
+		
+		layer.on('featureOver', function(e, pos, latlng, data) {
+			cartodb.log.log(e, pos, latlng, data);
+		});
+		
+		layer.on('error', function(err) {
+			cartodb.log.log('error: ' + err);
+		});
+	}).on('error', function() {
+		cartodb.log.log("some error occured");
+	});
+	
+	var zoomFS = new L.Control.ZoomFS();
+	map.addControl(zoomFS);
+	
+	map.on('enterFullscreen', function() {
+		console.log('fullscreen');
+		document.getElementById("legend").style.display = 'none';
+		document.getElementById("about").style.display = 'none';
+	});
+	
+	map.on('exitFullscreen', function() {
+		document.getElementById("legend").style.display = 'block';
+		document.getElementById("about").style.display = 'block';
+	});
+	
+    //var vis = cartodb.createVis('map', '//http://simeon.cartodb.com/api/v2/viz/46345544-6d62-11e3-99da-5404a6a69006/viz.json');
+}
